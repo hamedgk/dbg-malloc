efde6f0657a98539ad0ff4002940554ba2b989fa
Hamed Gandomkar <dev.hamedgk@yahoo.com>
2022-01-30 21:06:21 +0330

    deleted printing messages for dbg_malloc and dbg_free. and changed some variables to const

--- dbgalloc.c
+++ dbgalloc.c
@@ -1,7 +1,7 @@
 #include "dbgalloc.h"
 
 
-void *dbg_malloc(struct alloc_register_t *alloc_reg, size_t size, char const *file, size_t line){
+void *dbg_malloc(struct alloc_register_t *alloc_reg, size_t size, char const *file, size_t const line){
 	alloc_reg += reg_counter;
 	alloc_reg->line = line;
 	strcpy(alloc_reg->file_name, file);
@@ -9,26 +9,22 @@ void *dbg_malloc(struct alloc_register_t *alloc_reg, size_t size, char const *fi
 	void *alloc_ptr = malloc(size);
 	alloc_reg->allocation_address = alloc_ptr;
 	reg_counter++;
-	printf("allocation    %p in  %s:%ld\n",alloc_ptr, file, line);
 	return alloc_ptr;
 }
 
-void dbg_free(struct alloc_register_t *alloc_reg, void *to_be_freed){
-	for(int i=0; i<reg_counter; ++i){
+void dbg_free(struct alloc_register_t *alloc_reg, void * const to_be_freed){
+	for(int i=0; i<reg_counter; ++i)
 		if((alloc_reg+i)->allocation_address == to_be_freed){
 			free((alloc_reg+i)->allocation_address);
 			(alloc_reg+i)->is_deallocated = true;
-			printf("deacllocation %p for %s:%ld\n", (alloc_reg+i)->allocation_address, (alloc_reg+i)->file_name, (alloc_reg+i)->line);
 			return;
 		}
-
-	}
 }
 
 void not_freed(struct alloc_register_t *alloc_reg){
 	for(int i=0; i<reg_counter; ++i){
-		if((alloc_reg+i)->is_deallocated == false && (alloc_reg+i)->allocation_address != NULL){
-			printf("NOT FREED     %p in  %s:%ld\n", (alloc_reg+i)->allocation_address, (alloc_reg+i)->file_name
+		if((alloc_reg+i)->is_deallocated == false){
+			printf("NOT FREED %p at %s:%ld\n", (alloc_reg+i)->allocation_address, (alloc_reg+i)->file_name
 			, (alloc_reg+i)->line);
 		}
 	}
