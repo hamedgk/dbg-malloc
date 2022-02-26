3d9b5e4c38989a02150ae9e8945d99de30270752
Hamed Gandomkar <dev.hamedgk@yahoo.com>
2022-02-26 21:02:45 +0330

    Revert "added feature: invoke_callback for interating on deallocated blocks and run custom commands on them"
    This reverts commit 6614e1330c64240bf19876a117f28299108db201.

--- dbgalloc.h
+++ dbgalloc.h
@@ -22,8 +22,6 @@ void *dbg_malloc(struct alloc_register_t *alloc_reg, size_t size, char const *fi
 
 #define dbg_malloc_macro(alloc_register, size) dbg_malloc(alloc_register, size, __FILE__, __LINE__)
 
-void invoke_callback(struct alloc_register_t *alloc_reg, void (*callback)(struct alloc_register_t*));
-
 void dbg_free(struct alloc_register_t * const alloc_reg, void const * const to_be_freed);
 
 void not_freed(struct alloc_register_t * const alloc_reg);
