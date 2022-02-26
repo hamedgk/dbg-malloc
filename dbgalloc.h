8c81f4be4d63a7a6aadbb71bb728148f699ba8bf
Hamed Gandomkar <dev.hamedgk@yahoo.com>
2022-02-26 21:22:47 +0330

    more useful macros for eliminating boilerplates

--- dbgalloc.h
+++ dbgalloc.h
@@ -20,12 +20,18 @@ static size_t reg_counter = 0;
 
 void *dbg_malloc(struct alloc_register_t *alloc_reg, size_t size, char const *file, size_t const line);
 
-#define dbg_malloc_macro(alloc_register, size) dbg_malloc(alloc_register, size, __FILE__, __LINE__)
+#define dbg_malloc_macro(size) dbg_malloc(regs, size, __FILE__, __LINE__)
 
 void dbg_free(struct alloc_register_t * const alloc_reg, void const * const to_be_freed);
+
+#define dbg_free_macro(ptr_to_be_freed) dbg_free(regs, ptr_to_be_freed)
 
 void not_freed(struct alloc_register_t * const alloc_reg);
 
 #define not_freed_macro() not_freed(regs)
+
+void invoke_callback(struct alloc_register_t *alloc_reg, void (*callback) (struct alloc_register_t*));
+
+#define invoke_callback_macro(callback) invoke_callback(regs, callback)
 
 #endif
